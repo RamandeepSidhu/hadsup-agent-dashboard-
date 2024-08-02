65756f80a5f66b6b3431a082a8c4dfb8fd868843
RamandeepSidhu <117151940+RamandeepSidhu@users.noreply.github.com>
2024-08-02 12:46:42 +0530

    custom menu outside click then close

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'hadsup-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
-  isMenuOpen:boolean =false;
+  @ViewChild('toggleButton') toggleButton: ElementRef | any;
+  @ViewChild('menu') menu: ElementRef | any;
+  isMenuOpen: boolean = false;
   isSettingsMenuOpen = false;
-
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (
+        e.target !== this.toggleButton.nativeElement &&
+        e.target !== this.menu.nativeElement
+      ) {
+        this.isSettingsMenuOpen = false;
+      }
+    });
+  }
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
@@ -21,7 +32,7 @@ export class NavBarComponent {
   }
 
   logout(): void {
-    this.isSettingsMenuOpen=false;
+    this.isSettingsMenuOpen = false;
     console.log('Logout clicked');
   }
 }
